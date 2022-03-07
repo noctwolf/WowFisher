b1a067b549c89aaa264fdd76e7834506be0e6a27
noctwolf <noctwolf@gmail.com>
2022-03-07 10:30:22 +0800

    添加项目文件。

---
+++ WowFisher.Bot/WowProcess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using static Vanara.PInvoke.User32;
+
+namespace WowFisher.Bot
+{
+    public static class WowProcess
+    {
+        private static readonly Random random = new();
+        private static readonly List<string> names = new() { "Wow", "WowClassic", "Wow-64", "Notepad" };
+
+        public static bool IsWow(this Process process) => names.Contains(process.ProcessName);
+
+        public static bool IsWowClassic(this Process process) => process.ProcessName.Contains("Classic", StringComparison.OrdinalIgnoreCase);
+
+        public static void KeyPress(this Process process, ConsoleKey key)
+        {
+            if (!SetForegroundWindow(process.MainWindowHandle)) return;
+            INPUT[] inputs = new INPUT[2];
+            inputs[0].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[0].ki = new KEYBDINPUT { wVk = (ushort)key }; ;
+            inputs[1].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[1].ki = new KEYBDINPUT { wVk = (ushort)key, dwFlags = KEYEVENTF.KEYEVENTF_KEYUP };
+
+            if (SendInput((uint)inputs.Count(), inputs, Marshal.SizeOf(typeof(INPUT))) != inputs.Count())
+            {
+            }
+        }
+
+        public static void MouseRightClick(this Process process, Point point)
+        {
+            if (!SetForegroundWindow(process.MainWindowHandle)) return;
+            //if (!SetCursorPos(point.X, point.Y)) return;
+            mouse_event(MOUSEEVENTF.MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF.MOUSEEVENTF_RIGHTUP, 0, 0, 0, (IntPtr)0);
+        }
+
+        public static Process[] GetWowProcesses() => Process.GetProcesses().AsEnumerable().Where(f => f.IsWow()).ToArray();
+
+    }
+}
---
+++ WowFisher.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WowFisher.Bot;
+using static Vanara.PInvoke.User32;
+
+namespace WowFisher.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var wow = WowProcess.GetWowProcesses();
+            wow[0].KeyPress(ConsoleKey.D1);
+            wow[0].KeyPress(ConsoleKey.D2);
+            wow[0].KeyPress(ConsoleKey.D3);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var wow = WowProcess.GetWowProcesses();
+            wow[0].MouseRightClick(new System.Drawing.Point(0, 0));
+
+        }
+    }
+}
